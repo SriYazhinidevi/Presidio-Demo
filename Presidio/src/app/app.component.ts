601f951cf9189d5ce4d187594c159b2db102a5aa
SriYazhinidevi <sriyazhinidevi12@gmail.com>
2022-06-20 21:10:47 +0530

    added splashscreen

--- Presidio/src/app/app.component.ts
+++ Presidio/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Plugins } from '@capacitor/core';
 import { NavController, Platform } from '@ionic/angular';
 import { AuthenticationService } from './services/authentication.service';
+import { ModalController } from '@ionic/angular';
+import { SplashComponent } from './splash/splash.component';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -10,8 +12,8 @@ import { AuthenticationService } from './services/authentication.service';
 export class AppComponent {
   constructor(private auth: AuthenticationService,
     private navController: NavController,
-    private platform: Platform) {
-      this.initializeApp();
+    private platform: Platform,public modalController: ModalController) {
+      this.presentSplash();
 
     this.auth.loginStatusChanged.subscribe(authenticated => 
          this.handleAuthChange(authenticated));
@@ -21,6 +23,13 @@ export class AppComponent {
       if (this.platform.is('hybrid')) {
         await SplashScreen.hide();
       }
+    }
+    async presentSplash() {
+      const modal = await this.modalController.create({
+        component: SplashComponent,
+        cssClass: 'my-custom-class'
+      });
+      return await modal.present();
     }
     private handleAuthChange(authenticated: boolean) {
       if (authenticated) {
